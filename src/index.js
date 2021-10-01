bb43f6e582370e5199d8c53c0637f9cea3fa2266
teemutin <80262041+teemutin@users.noreply.github.com>
2021-10-01 14:33:57 +0300

    1 redi

--- src/index.js
+++ src/index.js
@@ -11,8 +11,20 @@ if (document.readyState !== "loading") {
 function initializeCode() {
   /*document.getElementById("app").innerHTML = "<h1>Hello!</h1>";*/
   let newDiv = document.createElement("div");
+  newDiv.classList.add("wiki-item");
+  document.body.appendChild(newDiv);
+
   let newHeader = document.createElement("h1");
-  console.log(newDiv);
+  newHeader.classList.add("wiki-header");
+  newDiv.appendChild(newHeader);
+
+  let newDiv2 = document.createElement("div");
+  newDiv2.classList.add("wiki-content");
+
+  let newPar = document.createElement("p");
+  newPar.classList.add("wiki-text");
+  newDiv2.appendChild(newPar);
+
   console.log(newHeader);
-  newDiv.classList.add("Luokka");
+  console.log(newPar);
 }
